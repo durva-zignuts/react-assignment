2863a5662c5575d025a46ffbbae548ec8e887563
Kapil Motwani <motwanikapil1@gmail.com>
2023-10-12 18:41:37 +0530

    Basic Signup Created

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,7 +1,105 @@
-import React from 'react'
+import React from "react";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import "./Signup.css";
+import "yup-phone";
 
 export default function SignupPage() {
+  const onSubmit = (data) => {
+    console.log("Form submitted");
+    console.log(data);
+  };
+
+  const schema = yup.object().shape({
+    firstName: yup.string().required(),
+    lastName: yup.string().required(),
+    mobileNumber: yup.number().required(),
+    email: yup.string().email().required(),
+    password: yup.string().required().max(20).min(4),
+    confirmPassword: yup.string().oneOf([yup.ref("password")], null),
+  });
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
   return (
-    <div>SignupPage</div>
-  )
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)} className="signupForm">
+        <div className="input-div">
+          <label htmlFor="firstName">First Name</label>
+          <input
+            type="text"
+            placeholder="First Name"
+            {...register("firstName")}
+            name="firstName"
+          />
+        </div>
+        {errors.firstName && (
+          <h4 className="error">{errors.firstName.message}</h4>
+        )}
+        <div className="input-div">
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            type="text"
+            placeholder="Last Name"
+            name="lastName"
+            {...register("lastName")}
+          />
+        </div>
+        {errors.lastName && (
+          <h4 className="error">{errors.lastName.message}</h4>
+        )}
+        <div className="input-div">
+          <label htmlFor="mobileNumber">Mobile Number</label>
+          <input
+            type="number"
+            placeholder="Mobile Number"
+            name="mobileNumber"
+            {...register("mobileNumber")}
+          />
+        </div>
+        {errors.mobileNumber && (
+          <h4 className="error">{errors.mobileNumber.message}</h4>
+        )}
+        <div className="input-div">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            {...register("email")}
+          />
+        </div>
+        {errors.email && <h4 className="error">{errors.email.message}</h4>}
+        <div className="input-div">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            {...register("password")}
+          />
+        </div>
+        {errors.password && (
+          <h4 className="error">{errors.password.message}</h4>
+        )}
+        <div className="input-div">
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            {...register("confirmPassword")}
+          />
+        </div>
+        {errors.confirmPassword && <h4>{errors.confirmPassword.message}</h4>}
+        <button type="submit">Submit</button>
+        {console.log("errors : ", errors)}
+      </form>
+    </div>
+  );
 }
