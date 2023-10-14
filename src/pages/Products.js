c2ace45256c02da96f0caeb90fb157f92a9c2060
durvabanthia <durvarbanthia@gmail.com>
2023-10-14 17:50:00 +0530

    added pagination

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,77 @@
+// import { Link } from "react-router-dom"
+// import Navbar from "../components/Navbar"
+// import { useEffect, useState } from "react"
+
+// const Products = () => {
+//   const [products, setProducts] = useState([])
+//   const [isLoading, setIsLoading] = useState(false)
+//   const [httpError, setHttpError] = useState()
+
+//   useEffect(() => {
+//     const fetchProducts = async () => {
+//       const response = await fetch("https://dummyjson.com/products")
+
+//       if (!response.ok) {
+//         throw new Error("Something went wrong!")
+//       }
+
+//       const responseData = await response.json()
+//       let data = responseData.products
+//       console.log(data)
+
+//       setProducts(data)
+//       setIsLoading(false)
+//     }
+
+//     fetchProducts().catch((error) => {
+//       setIsLoading(false)
+//       setHttpError(error.message)
+//     })
+//   }, [])
+
+//   if (isLoading) {
+//     return (
+//       <section>
+//         <p>Loading...</p>
+//       </section>
+//     )
+//   }
+
+//   if (httpError) {
+//     return (
+//       <section>
+//         <p>{httpError}</p>
+//       </section>
+//     )
+//   }
+
+//   return (
+//     <div>
+//       <Navbar />
+//       Products Page:
+//       <div>
+//         <ul>
+//           {products.map((product) => (
+//             <Link to={`${product.id}`}>
+//               <li key={product.id}>{product.title}</li>
+//             </Link>
+//           ))}
+//         </ul>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Products
+
 import { Link } from "react-router-dom"
 import Navbar from "../components/Navbar"
 import { useEffect, useState } from "react"
 
 const Products = () => {
   const [products, setProducts] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const productsPerPage = 8
   const [isLoading, setIsLoading] = useState(false)
   const [httpError, setHttpError] = useState()
 
@@ -17,7 +85,6 @@ const Products = () => {
 
       const responseData = await response.json()
       let data = responseData.products
-      console.log(data)
 
       setProducts(data)
       setIsLoading(false)
@@ -28,6 +95,18 @@ const Products = () => {
       setHttpError(error.message)
     })
   }, [])
+
+  // Calculate the index range for the current page
+  const indexOfLastProduct = currentPage * productsPerPage
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage
+  const currentProducts = products.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  )
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber)
+  }
 
   if (isLoading) {
     return (
@@ -48,15 +127,26 @@ const Products = () => {
   return (
     <div>
       <Navbar />
-      Products Page:
+      <h2>Products Page:</h2>
       <div>
         <ul>
-          {products.map((product) => (
-            <Link to={`${product.id}`}>
-              <li key={product.id}>{product.title}</li>
+          {currentProducts.map((product) => (
+            <Link to={`/${product.id}`} key={product.id}>
+              <li>{product.title}</li>
             </Link>
           ))}
         </ul>
+      </div>
+
+      {/* Pagination */}
+      <div className="pagination">
+        {Array.from({
+          length: Math.ceil(products.length / productsPerPage),
+        }).map((_, index) => (
+          <button key={index + 1} onClick={() => paginate(index + 1)}>
+            {index + 1}
+          </button>
+        ))}
       </div>
     </div>
   )
